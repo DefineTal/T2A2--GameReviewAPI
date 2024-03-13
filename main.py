6a6b11e2072f10e8a92b3b56d0cbae67a8c3bdbe
DefineTal <taliesinstudy@gmail.com>
2024-03-13 19:12:20 +1100

    create main, init and config files

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from init import db, ma, bcrypt, jwt 
+
+# Function to create the app instance
+def create_app():
+    app = Flask(__name__)
+
+    # config
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URI")
+    app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+
+    # connect libaries from init to the instance
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
+    
