3400565fc42479b0618307b6f628e429111fcfde
DefineTal <taliesinstudy@gmail.com>
2024-03-17 15:09:27 +1100

    add login route

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -3,6 +3,8 @@ from init import db, bcrypt
 from models.user import User, user_schema
 from psycopg2 import errorcodes
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth/')
 
@@ -39,4 +41,21 @@ def auth_register():
             return {"error": f"{err.orig.diag.column_name} is null, make sure to input a value!"}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Username is taken!"}, 409
+
+
+@auth_bp.route("/login", methods=['POST'])
+def auth_login():
+    # convert body data to  python obj and search for matching username field
+    body_data = request.get_json()
+    stmt = db.Select(User).filter_by(username=body_data.get("username"))
+    user = db.session.scalar(stmt)
+
+    # check if details match else return error
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        #create jwt which expires after 24 hours
+        jwt = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {"username": user.username, "jwt": jwt, "is_admin": user.is_admin}
     
+    else:
+        error = {"error": "email and password combination is incorrect"}
+        return error, 401
